fa3b6485ef354cac77b7cf821047042a6460b9b4
Benjamin <bnjackson951@gmail.com>
2023-10-13 13:26:56 +0100

    Add functionality to get and check user account data

--- utils/input.js
+++ utils/input.js
@@ -3,6 +3,7 @@ const readlineSync = require('readline-sync');
 function getUserInput(question, choices) {
     const userInput = readlineSync.question(`${question} `).toLowerCase();
     return checkUserInput(userInput) ? userInput : getUserInput(question, choices);
+
     function checkUserInput(userInput) {
         if (choices.includes(userInput)) {
             return true;
@@ -13,6 +14,30 @@ function getUserInput(question, choices) {
     }
 }
 
+function getUserAccountInfo(question) {
+    const userInput = readlineSync.question(`${question} `);
+    return checkUserInput(userInput) ? userInput : getUserAccountInfo(question);
+
+    function checkUserInput(userInput) {
+        const nonSpacePattern = /\S+/;
+        // Checks that string contains at least one non space character
+        if (nonSpacePattern.test(userInput)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+// function getUserAccountChoice(accounts) {
+//     const userChoice = readlineSync.question('What ')
+//     function outputAccountChoices(Accounts) {
+
+//     }
+// }
+
 module.exports = {
-    getUserInput
+    getUserInput,
+    getUserAccountInfo,
+    getUserAccountChoice
 }
