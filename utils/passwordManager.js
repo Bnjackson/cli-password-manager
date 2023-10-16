dd47c45da5c2b536138700869633be778a9585b6
Benjamin <bnjackson951@gmail.com>
2023-10-16 19:23:52 +0100

    Add functionality to verify user inputted password an compare to hashed password

--- utils/passwordManager.js
+++ utils/passwordManager.js
@@ -3,16 +3,24 @@ const bcrypt = require('bcrypt');
 async function encryptPassword(account) {
     try {
         const hash = await bcrypt.hash(account.password, 10);
-        account.password = hash;
-        console.log(hash, account);    
+        account.password = hash; 
     } catch (err) {
         console.error(`Hashing password failed: ${err}`);
     }
     return account;
 }
 
-function verifyPassword(password, account) {
-
+async function verifyPassword(userEnteredPassword, account) {
+    try {
+        const match = await bcrypt.compare(userEnteredPassword, account.password);
+        if (match) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (err) {
+        console.error('Error comparing passwords', err);
+    }
 }
 
 module.exports = {
