a6683d597f64a89afe6c461e74c17c5d67822d55
Benjamin <bnjackson951@gmail.com>
2023-10-16 19:27:53 +0100

    Add functionality to get user account choice, get user password choice, compare user password choice with stored hashed password, display account with original password

---
+++ commands/display.js
@@ -0,0 +1,35 @@
+const databaseModule = require('../utils/database.js');
+const inputModule = require('../utils/input.js');
+const passwordModule = require('../utils/passwordManager.js');
+
+async function getAccountToDisplay() {
+    const accounts = await databaseModule.getAccountsData();
+    if (accounts.length < 1) {
+        console.log(`Accounts is empty so cannot be display`);
+    } else {
+        const userAccountChoice = await inputModule.getUserAccountChoice(accounts);
+        await getPasswordForAccount(userAccountChoice);
+    }
+}
+
+async function getPasswordForAccount(account) {
+    const password = await inputModule.getUserPasswordInput();
+    await checkUserPasword(password, account);
+    async function checkUserPasword(password, account) {
+        if (await passwordModule.verifyPassword(password, account)) {
+            account.password = password;
+            await displayAccount(account);
+        } else {
+            console.log('Password inputted does not match hashed password. Try again');
+            getPasswordForAccount(account);
+        }
+    }
+}
+
+async function displayAccount(account) {
+    console.log(account);
+}
+
+module.exports = {
+    getAccountToDisplay
+}
