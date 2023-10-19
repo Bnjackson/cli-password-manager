73930b0db1f4388a9a7cf2b4edbd19b8bcc3dc00
Benjamin <bnjackson951@gmail.com>
2023-10-19 16:40:07 +0100

    Add jsonSpace as a seperate variable, fix formatiing for writeAccountsFile error message

--- utils/database.js
+++ utils/database.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 
 const filePath = './data/accounts.json';
+const jsonSpace = 2;
 
 async function getAccountsData() {
     try {
@@ -14,11 +15,12 @@ async function getAccountsData() {
 
 async function writeAccountsFile(updatedAccounts) {
     try {
-        const data = JSON.stringify(updatedAccounts, null, 2);
+        const data = JSON.stringify(updatedAccounts, null, jsonSpace);
         await fs.writeFileSync(filePath, data, 'utf8');
         console.log('Accounts successfully written');
     } catch (error) {
-        console.error(`Error adding account to accounts file:${error}`)
+        console.error(`Error adding account to accounts file:
+        ${error}`)
     }
 }
 
